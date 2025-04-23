584048057e903b00cb37bd2af6b3c83e4d2adc63
rushikesh-void <rushibalwant58@gmail.com>
2025-04-23 18:25:26 +0530

    second  repository

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user"
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet"
+// export const USER_API_END_POINT = "http://localhost:8080/api/v1/user"
+// export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet"
+
+export const USER_API_END_POINT="https://final-twitter-backend-3g44.onrender.com/api/v1/user"
+export const TWEET_API_END_POINT="https://final-twitter-backend-3g44.onrender.com/api/v1/tweet"
