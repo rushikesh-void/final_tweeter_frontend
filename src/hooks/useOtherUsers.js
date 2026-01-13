e01fe300b351f7734f5544c9c0b4a615571ae375
rushikesh-void <rushibalwant58@gmail.com>
2026-01-13 12:22:36 +0530

    500 error fixed

--- src/hooks/useOtherUsers.js
+++ src/hooks/useOtherUsers.js
@@ -1,25 +1,37 @@
 import axios from "axios";
 import { USER_API_END_POINT } from "../utils/constant";
 import { useEffect } from "react";
-import {useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import { getOtherUsers } from "../redux/userSlice";
-// import { getMyProfile, getOtherUsers } from "../redux/userSlice";
 
 const useOtherUsers = (id) => {
-    const dispatch = useDispatch();
-    useEffect(()=>{
-        const fetchOtherUsers = async () => {
-            try {
-                const res = await axios.get(`${USER_API_END_POINT}/otheruser/${id}`,{
-                    withCredentials:true
-                });
-                console.log(res);
-                dispatch(getOtherUsers(res.data.otherUsers));
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchOtherUsers();
-    },[dispatch,id]);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // ✅ STOP API CALL IF ID IS NOT READY (login / logout case)
+    if (!id) {
+      dispatch(getOtherUsers(null)); // clear other users on logout
+      return;
+    }
+
+    const fetchOtherUsers = async () => {
+      try {
+        const res = await axios.get(
+          `${USER_API_END_POINT}/otheruser/${id}`,
+          { withCredentials: true }
+        );
+
+        dispatch(getOtherUsers(res.data.otherUsers));
+      } catch (error) {
+        console.error(
+          "Fetch other users error:",
+          error.response?.data || error.message
+        );
+      }
+    };
+
+    fetchOtherUsers();
+  }, [id, dispatch]);
 };
+
 export default useOtherUsers;
--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,33 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-    name: "user",
-    initialState: {
-        user: null,
-        otherUsers: null,
-        profile: null
+  name: "user",
+  initialState: {
+    user: null,
+    otherUsers: null,
+    profile: null,
+  },
+  reducers: {
+    getUser: (state, action) => {
+      state.user = action.payload;
     },
-    reducers: {
-        getUser: (state, action) => {
-            state.user = action.payload;
-        },
-        getOtherUsers: (state, action) => {
-            state.otherUsers = action.payload;
-        },
-        getMyProfile: (state, action) => {
-            state.profile = action.payload;
-        },
-        followingUpdate:(state, action) =>{
-            if (state.user.following.includes(action.payload)){
-                state.user.following = state.user.following.filter((itemId)=>{
-                    return itemId !== action.payload;
-                })
-            }else{
-                state.user.following.push(action.payload);
-            }
-        }
-    }
+
+    getOtherUsers: (state, action) => {
+      state.otherUsers = action.payload;
+    },
+
+    getMyProfile: (state, action) => {
+      state.profile = action.payload;
+    },
+
+    followingUpdate: (state, action) => {
+      // ✅ SAFETY CHECK (prevents crash after logout)
+      if (!state.user || !state.user.following) return;
+
+      if (state.user.following.includes(action.payload)) {
+        state.user.following = state.user.following.filter(
+          (itemId) => itemId !== action.payload
+        );
+      } else {
+        state.user.following.push(action.payload);
+      }
+    },
+  },
 });
 
-export const {getUser,getOtherUsers, getMyProfile, followingUpdate} = userSlice.actions;
+export const {
+  getUser,
+  getOtherUsers,
+  getMyProfile,
+  followingUpdate,
+} = userSlice.actions;
+
 export default userSlice.reducer;
